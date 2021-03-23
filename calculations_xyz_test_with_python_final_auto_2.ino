7db14cfbcd75320249ef2c437828bbc7bd1621f5
05jayesh <41137643+05jayesh@users.noreply.github.com>
2021-03-23 09:03:18 +0530

    Add files via upload

---
+++ calculations_xyz_test_with_python_final_auto_2.ino
@@ -0,0 +1,495 @@
+#include<Servo.h>
+#include<Wire.h>
+#include<Math.h>
+
+
+Servo s1;
+Servo s2;
+Servo s3;
+Servo s4;
+Servo s5;
+Servo s6;
+
+int d=15;//servo step delay
+int i=0;
+int p1=90;
+int p2=0;
+int p3=90;
+int p4=0;
+int p5=90;
+int p6=10;//open
+
+int ppd=-10;//grippper default world angle
+
+//positions in calculations
+int P1=0;
+int P2=0;
+int P3=90;
+int P4=0;
+int P5=90;
+int P6=10;
+
+
+
+//trigonometery
+//coc
+double pp1c=0;
+double pp2c=0;
+double pp3c=0;
+double pp4c=0;
+double pp5c=0;
+double pp6c=0;
+double ppdc=0;
+
+//sine
+double pp1s=0;
+double pp2s=0;
+double pp3s=0;
+double pp4s=0;
+double pp5s=0;
+double pp6s=0;
+double ppds=0;
+
+
+
+//coordinates
+
+//gripper coordinates
+float X=0;
+float Y=0;
+float Z=0;
+
+//pick up coordinates
+float Xi=29;
+float Yi=12;
+float Zi=15;
+
+//place coordinates
+float Xo=0;
+float Yo=0;
+float Zo=0;
+
+//arm lengths
+float x1=11.25;//base height
+float x2=9;//humerous
+float x3=8.25;//ulna
+float x4=18.75;//wrist & gripper
+float cf=1.5;//correction factor
+
+int incoming[6];
+
+int type;
+int Xm=0;
+int Ym=0;
+int Zm=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin(9600);
+  Serial.begin(9600);
+
+  s1.attach(8);
+  s2.attach(2);
+  s3.attach(11);
+  s4.attach(13);
+  s5.attach(5);
+  s6.attach(9);
+
+  s1.write(p1);
+  s2.write(p1);
+  s3.write(p3);
+  s4.write(p4);
+  s5.write(p5);
+  s6.write(p6);
+  
+ 
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+    total_reset();
+
+    delay(1000);
+    
+         while (Serial.available()>=4)
+         {
+          for(int i=0;i<7;i++)
+          {
+            incoming[i]=Serial.read();
+          }
+          Xi=incoming[0];
+          Yi=incoming[1];
+          Zi=incoming[2];
+          type=incoming[3];
+          Xm=incoming[4];
+          Ym=incoming[5];
+          Zm=incoming[6];
+          if (Xm==1)
+          {
+            Xi=-Xi;
+          }
+          if (Ym==1)
+          {
+            Yi=-Yi;
+          }
+          if (Zm==1)
+          {
+            Zi=-Zi;
+          }
+
+          play();
+         }
+        
+
+}
+
+
+void action()
+{
+  for(i=0;i<180;i++)
+  {
+    if (p1<P1)
+    {
+      p1=p1+1;
+      s1.write(p1);
+    }
+    if (p1>P1)
+    {
+      p1=p1-1;
+      s1.write(p1);
+    }
+
+    if (p2<P2)
+    {
+      p2=p2+1;
+      s2.write(p2);
+    }
+    if (p2>P2)
+    {
+      p2=p2-1;
+      s2.write(p2);
+    } 
+
+    if (p3<P3)
+    {
+      p3=p3+1;
+      s3.write(p3);
+    }
+    if (p3>P3)
+    {
+      p3=p3-1;
+      s3.write(p3);
+    }
+
+    if (p4<P4)
+    {
+      p4=p4+1;
+      s4.write(p4);
+    }
+    if (p4>P4)
+    {
+      p4=p4-1;
+      s4.write(p4);
+    }
+
+    if (p5<P5)
+    {
+      p5=p5+1;
+      s5.write(p5);
+    }
+    if (p5>P5)
+    {
+      p5=p5-1;
+      s5.write(p5);
+    }
+
+
+    delay (d);
+    s5.write(90);
+  }
+}
+
+ void total_reset()
+ {
+  s1.write(90);
+  s2.write(0);
+  s3.write(90);
+  s4.write(0);
+  s5.write(90);
+  s6.write(10);
+ }
+
+
+
+ void reset()
+ {
+  for(i=0;i<30;i++)
+  {
+    if(p4<P4+30)
+    {
+      p4=p4+1;
+      s4.write(p4);
+    }
+    if(p4>P4+30)
+    {
+      p4=p4-1;
+      s4.write(p4);
+    }
+
+    delay(d);
+  }
+
+  for(i=0;i<180;i++)
+  {
+    if(p1<90)
+    {
+      p1=p1+1;
+      s1.write(p1);
+    }
+    if(p1>90)
+    {
+      p1=p1-1;
+      s1.write(p1);
+    }
+
+    if(p2<0)
+    {
+      p2=p2+1;
+      s2.write(p2);
+    }
+    if(p2>0)
+    {
+      p2=p2-1;
+      s2.write(p2);
+    }
+
+    if(p3<90)
+    {
+      p3=p3+1;
+      s3.write(p3);
+    }
+    if(p3>90)
+    {
+      p3=p3-1;
+      s3.write(p3);
+    }
+
+    if(p4<0)
+    {
+      p4=p4+1;
+      s4.write(p4);
+    }
+    if(p4>0)
+    {
+      p4=p4-1;
+      s4.write(p4);
+    }
+    s5.write(90);
+    delay(d);
+  }
+
+ }
+
+ void calculate_position()
+ {
+      ppds=sin(ppd*PI/180);
+      ppdc=cos(ppd*PI/180);
+      
+      double d=atan(Yi/Xi);
+      if(Xi<0)
+      {
+        P1=-(d*180/PI);
+      }
+      if(Xi>0)
+      {
+        P1=180-(d*180/PI);
+      }
+      if(Xi==0)
+      {
+        P1=90;
+      }
+      Zi=Zi-2;
+      
+
+      float base=((sqrt((Xi*Xi)+(Yi*Yi)))-(x4*ppdc))+cf;
+      float height=Zi-(x1+(x4*ppds));
+      float hype=sqrt((base*base)+(height*height));
+
+      if(base>0)
+      {
+        double Q1=(atan(height/base))*180/PI;
+        double Q2=(acos(((hype*hype)+(x2*x2)-(x3*x3))/(2*hype*x2)))*180/PI;
+        double Q3=(acos(((hype*hype)-(x2*x2)-(x3*x3))/(2*x2*x3)))*180/PI;
+        P2=180-(Q1+Q2);
+        P3=Q3;
+        P4=P2+P3-ppd-90;
+
+      
+      
+        pp1c=cos(P1*PI/180);
+        pp1s=sin(P1*PI/180);
+        pp2c=cos(P2*PI/180);
+        pp2s=sin(P2*PI/180);
+        pp3c=cos((P3+(P2-90))*PI/180);
+        pp3s=sin((P3+(P2-90))*PI/180);
+        pp4c=cos((P4-((P3+(P2-90))-90))*PI/180);
+        pp4s=sin((P4-((P3+(P2-90))-90))*PI/180);
+
+        int ppp2=180-P2;
+        int ppp3=270-P2-P3;
+
+        double ppp2c=cos(ppp2*PI/180);
+        double ppp3c=cos(ppp3*PI/180);
+        double ppp2s=sin(ppp2*PI/180);
+        double ppp3s=sin(ppp3*PI/180);
+
+     
+
+        X=((x2*ppp2c)+(x3*ppp3c)+(x4*ppdc))*pp1c;
+        Y=((x2*ppp2c)+(x3*ppp3c)+(x4*ppdc))*pp1s;
+        Z=x1+(x2*ppp2s)+(x3*ppp3s)+(x4*ppds);
+
+      }
+
+      if (base<0)
+      {
+        double Q1=(atan(height/base))*180/PI;
+        double Q2=(acos(((hype*hype)+(x2*x2)-(x3*x3))/(2*hype*x2)))*180/PI;
+        double Q3=(acos(((hype*hype)-(x2*x2)-(x3*x3))/(2*x2*x3)))*180/PI;
+        P2=Q1-Q2;
+        P3=Q3;
+        P4=P2+P3-ppd-90;
+
+      
+      
+        pp1c=cos(P1*PI/180);
+        pp1s=sin(P1*PI/180);
+        pp2c=cos(P2*PI/180);
+        pp2s=sin(P2*PI/180);
+        pp3c=cos((P3+(P2-90))*PI/180);
+        pp3s=sin((P3+(P2-90))*PI/180);
+        pp4c=cos((P4-((P3+(P2-90))-90))*PI/180);
+        pp4s=sin((P4-((P3+(P2-90))-90))*PI/180);
+
+        int ppp2=180-P2;
+        int ppp3=270-P2-P3;
+
+        double ppp2c=cos(ppp2*PI/180);
+        double ppp3c=cos(ppp3*PI/180);
+        double ppp2s=sin(ppp2*PI/180);
+        double ppp3s=sin(ppp3*PI/180);
+
+     
+
+        X=((-x2*pp2c)+(x3*ppp3c)+(x4*ppdc))*pp1c;
+        Y=((-x2*pp2c)+(x3*ppp3c)+(x4*ppdc))*pp1s;
+        Z=x1+(x2*pp2s)+(x3*ppp3s)+(x4*ppds);
+        
+      }
+   
+         Serial.print("X= ");
+         Serial.print(X);
+         Serial.print("  |Y= ");
+         Serial.print(Y);
+         Serial.print("  |Z= ");
+         Serial.print(Z);
+         Serial.print("  |P1= ");
+         Serial.print(P1);
+         Serial.print("  |P2= ");
+         Serial.print(P2);
+         Serial.print("  |P3= ");
+         Serial.print(P3);
+         Serial.print("  |P4= ");
+         Serial.print(P4);
+         Serial.println();
+ }
+
+ void play()
+ {
+         calculate_position();
+         delay(500);
+         if (type==1)
+         {
+         action();
+         delay(500);
+         for(p6=9;p6<=170;p6++)//close
+         {
+          s6.write(p6);
+         }
+         delay(500);
+      
+        
+         Xi=-29;
+         Yi=8;
+         Zi=10;
+
+         calculate_position();
+         delay(500);
+         action();
+         delay(500);
+         for(p6=171;p6>=10;p6--)//open
+         {
+          s6.write(p6);
+         }
+         delay(500);
+         reset();
+         delay(500);
+
+         }
+
+         if (type==2)
+         {
+         action();
+         delay(500);
+         for(p6=9;p6<=170;p6++)//close
+         {
+          s6.write(p6);
+         }
+         delay(500);
+      
+          
+          Xi=29;
+          Yi=8;
+          Zi=10;
+
+         calculate_position();
+         delay(500);
+         action();
+         delay(500);
+         for(p6=171;p6>=10;p6--)//open
+         {
+          s6.write(p6);
+         }
+         delay(500);
+         
+
+         delay(500);
+         reset();
+         delay(500);
+  
+         }
+         
+
+         Xi=incoming[0];
+         Yi=incoming[1];
+         Zi=incoming[2];
+         type=incoming[3];
+          Xm=incoming[4];
+          Ym=incoming[5];
+          Zm=incoming[6];
+          if (Xm==1)
+          {
+            Xi=-Xi;
+          }
+          if (Ym==1)
+          {
+            Yi=-Yi;
+          }
+          if (Zm==1)
+          {
+            Zi=-Zi;
+          }
+
+ }
+
---
+++ stereo_test_with_python_2_auto.py
@@ -0,0 +1,147 @@
+import numpy as np
+import cv2
+import math
+import struct
+import time
+import serial
+
+ser1=serial.Serial('com7',9600)
+
+time.sleep(2)
+
+detect_cascade=cv2.CascadeClassifier('haarcascade_a.xml')
+camR=cv2.VideoCapture(2)
+camR.set(cv2.CAP_PROP_FRAME_WIDTH,720)
+camR.set(cv2.CAP_PROP_FRAME_HEIGHT,405)
+camL=cv2.VideoCapture(1)
+camL.set(cv2.CAP_PROP_FRAME_WIDTH,720)
+camL.set(cv2.CAP_PROP_FRAME_HEIGHT,405)
+
+X=0
+Y=0
+Z=0
+
+XX=0
+YY=0
+ZZ=0
+
+c=0
+c1=0
+c2=0
+c3=0
+c4=0
+c5=0
+c6=0
+c7=0
+c8=0
+c9=0
+c10=0
+c11=0
+
+while(True):
+    a1=2000 #right
+    a2=2000 #left
+    tfR,frameR=camR.read()
+    gray = cv2.cvtColor(frameR, cv2.COLOR_BGR2GRAY)
+    detect = detect_cascade.detectMultiScale(gray, 1.3, 5)
+    for (xrr,yrr,wr,hr) in detect:
+        cv2.rectangle(frameR,(xrr,yrr),(xrr+wr,yrr+hr),(255,0,0),2)
+        cv2.putText(frameR, "A",(xrr,yrr-50), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(255,0,0),2);
+        cv2.putText(frameR, "X=" + `X`,(xrr,yrr-30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2);
+        cv2.putText(frameR, "Y=" + `Y`,(xrr,yrr-15), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2);
+        cv2.putText(frameR, "Z=" + `Z`,(xrr,yrr), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2);
+
+        cv2.putText(frameR, "XX=" + `XX`,(xrr,(yrr+hr)+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2);
+        cv2.putText(frameR, "YY=" + `YY`,(xrr,(yrr+hr)+30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2);
+        cv2.putText(frameR, "ZZ=" + `ZZ`,(xrr,(yrr+hr)+45), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2);
+
+        xr=xrr+(wr/2)
+        yr=yrr+(hr/2)
+        a1=(50*math.tan(32.5*3.14/180)*(xr-360)/360)-3.4
+        hr=(50*math.tan(17*3.14/180)*(yr-202.5)/202.5)
+        
+        
+    cv2.imshow('frame_Right',frameR)
+
+    tfL,frameL=camL.read()
+    gray = cv2.cvtColor(frameL, cv2.COLOR_BGR2GRAY)
+    detect = detect_cascade.detectMultiScale(gray, 1.3, 5)
+    for (xll,yll,wl,hl) in detect:
+        cv2.rectangle(frameL,(xll,yll),(xll+wl,yll+hl),(255,0,0),2)
+        cv2.putText(frameL, "A",(xll,yll-50), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(255,0,0),2);
+        cv2.putText(frameL, "X=" + `X`,(xll,yll-30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2);
+        cv2.putText(frameL, "Y=" + `Y`,(xll,yll-15), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2);
+        cv2.putText(frameL, "Z=" + `Z`,(xll,yll), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2);
+
+        cv2.putText(frameL, "XX=" + `XX`,(xll,(yll+hl)+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2);
+        cv2.putText(frameL, "YY=" + `YY`,(xll,(yll+hl)+30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2);
+        cv2.putText(frameL, "ZZ=" + `ZZ`,(xll,(yll+hl)+45), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2);
+
+        xl=xll+(wl/2)
+        yl=yll+(hl/2)
+        a2=(50*math.tan(32.5*3.14/180)*(xl-360)/360)+3.4
+        hl=(50*math.tan(17*3.14/180)*(yl-202.5)/202.5)
+        
+        
+    cv2.imshow('frame_Left',frameL)
+
+    if (a1<1000):
+        if (a2<1000):
+            x=((3.4*((-a2)-a1))/((-6.8)+a2-a1))
+            y=(50-(340/(6.8-a2+a1)))
+            X=-x
+            Y=-(50-y)
+            z=-(Y*hr)/50
+            Z=z
+
+            ZZ=Z+13
+            XX=38-(Y+1)
+            YY=X+28
+            
+            print 'X='
+            print X
+            print 'Y='
+            print Y
+            print 'Z='
+            print Z
+            
+            
+    if c11==1:
+        c10=c9
+        c9=c8
+        c8=c7
+        c7=c6
+        c6=c5
+        c5=c4
+        c4=c3
+        c3=c2
+        c2=c1
+        c1=c
+        c=XX+YY+ZZ
+
+        if c10>0:
+            if ((c10-c9)*(c10-c9))<=4:
+                if ((c10-c8)*(c10-c8))<=4:
+                    if ((c10-c7)*(c10-c7))<=4:
+                        if ((c10-c6)*(c10-c6))<=4:
+                            if ((c10-c5)*(c10-c5))<=4:
+                                if ((c10-c4)*(c10-c4))<=4:
+                                    if ((c10-c3)*(c10-c3))<=4:
+                                        if ((c10-c2)*(c10-c2))<=4:
+                                            if ((c10-c1)*(c10-c1))<=4:
+                                                if ((c10-c)*(c10-c))<=4:
+                                                    ser1.write(struct.pack('>BBBB',XX,YY,ZZ,TT))
+                                                    c11=0
+    
+    key=cv2.waitKey(1)
+
+    if key==ord('r'):
+        c11=1
+
+    
+    if key==ord('k'):
+            break
+     
+camR.release()
+camL.release()
+cv2.destroyAllWindows()
